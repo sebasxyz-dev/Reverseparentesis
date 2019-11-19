bbd7a87ded903f158137057d1b32f28b88e8dbc8
sebasxyz <sebas_krn@hotmail.com>
2019-11-19 09:55:33 -0500

    Add archives
    Se agregan los archivos

---
+++ subfunciones.js
@@ -0,0 +1,7 @@
+function apilar1(apilar){
+apilar.push([]);
+return apilar
+}
+
+
+exports.apilar1=apilar1;
---
+++ test.js
@@ -0,0 +1,48 @@
+const llamado = require('./subfunciones')
+
+chai = require ("chai");
+var expect = chai.expect;
+
+function StringParethesisReverse(cadena) {
+        let apilar = [[]];
+        for(let i=0; i<cadena.length; i++){
+            if(cadena[i] === "("){
+                apilar = llamado.apilar1(apilar);//.push([]);
+                console.log(apilar)
+            } else if (cadena[i] !== ")"){
+                apilar[apilar.length-1].push(cadena[i]);
+                console.log(apilar)
+            } else {
+                apilar[apilar.length-1].reverse();
+                console.log(apilar)
+                if(apilar[apilar.length-2]) apilar[apilar.length-2] = apilar[apilar.length-2].concat(apilar.pop());
+                console.log(apilar)
+            }
+        }
+        console.log(apilar[0].join(""));
+        return apilar[0].join("");
+        
+};
+
+
+
+
+describe ('StringParethesisReverse', function(){
+    
+    it ('Should return abcba when value is ab(abc)', function(){
+        var result = StringParethesisReverse("ab(abc)");
+        expect(result).to.equal('abcba')
+    });
+
+    it ('Should return 12345 when value is 12(543)', function(){
+        var result = StringParethesisReverse("12(543)");
+        expect(result).to.equal('12345')
+    }); 
+
+    
+    it ('Should return casoldra when value is cas(ar(old))', function(){
+        var result = StringParethesisReverse("cas(ar(old))");
+        expect(result).to.equal('casoldra')
+    }); 
+
+});
